3e7794367602f6d503640f3701765f32b4221d93
erick200011 <75103508+erick200011@users.noreply.github.com>
2023-12-05 00:10:11 -0500

    proyecto_moviles_funcionalidad_movil

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-      apiKey: "AIzaSyDsxerCFpDEXthHXWi-xr0zYY2mUuO6rMo",
-      authDomain: "moviles-ec43a.firebaseapp.com",
-      databaseURL: "https://moviles-ec43a-default-rtdb.firebaseio.com",
-      projectId: "moviles-ec43a",
-      storageBucket: "moviles-ec43a.appspot.com",
-      messagingSenderId: "170362843768",
-      appId: "1:170362843768:web:786dc0f49aac202370738d",
-      measurementId: "G-L5XHLL9Y9X",
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: "",
   
 },}
 
